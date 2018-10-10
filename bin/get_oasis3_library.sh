4c1fccf5f813ad4a54d0563e5577298a3b085eb9
Aidan Heerdegen <aidan.heerdegen@gmail.com>
2018-10-10 14:30:44 +1100

    Meaningless change to force travis rebuild

--- bin/get_oasis3_library.sh
+++ bin/get_oasis3_library.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
 TARFILE=$(curl -s https://api.github.com/repos/OceansAus/oasis3-mct/releases/latest | grep browser_download_url | cut -d: -f2- | tr -d ' "')
 wget ${TARFILE}
 tar -xzf $(basename ${TARFILE})
