dc08ec7b721597d5425e0c94a7f317d048fc8608
Aidan Heerdegen <aidan.heerdegen@gmail.com>
2018-10-10 12:17:35 +1100

    Changed to wget for download

--- bin/get_oasis3_library.sh
+++ bin/get_oasis3_library.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 TARFILE=$(curl -s https://api.github.com/repos/OceansAus/oasis3-mct/releases/latest | grep browser_download_url | cut -d: -f2- | tr -d ' "')
-curl -O ${TARFILE}
+wget ${TARFILE}
 tar -xzf $(basename ${TARFILE})
