40ac3cbd41ae93920715f8571d64650b226e73ea
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2018-10-10 22:20:20 +1100

    travis: fixed error in oasis script

--- bin/get_oasis3_library.sh
+++ bin/get_oasis3_library.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-[[ ${LIBACCESSOM2_ROOT} || ${OASIS_ROOT} ]] && git clone --depth=1 https://github.com/OceansAus/libaccessom2.git cd ${LIBACCESSOM2_ROOT} && mkdir build && cd build && cmake .. && make
+[[ ${LIBACCESSOM2_ROOT} || ${OASIS_ROOT} ]] && git clone --depth=1 https://github.com/OceansAus/libaccessom2.git && cd ${LIBACCESSOM2_ROOT} && mkdir build && cd build && cmake .. && make
