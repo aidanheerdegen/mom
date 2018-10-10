2e5a5540b75e004041f9cb90ccf47b62f075d34f
Aidan Heerdegen <aidan.heerdegen@gmail.com>
2018-10-10 12:06:19 +1100

    Refactored download script and made verbose

--- bin/get_oasis3_library.sh
+++ bin/get_oasis3_library.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-curl -O $(curl -s https://api.github.com/repos/OceansAus/oasis3-mct/releases/latest | grep browser_download_url | cut -d: -f2- | tr -d ' "')
-tar -xzf binary_release.tar.gz
+set -x
+TARFILE=$(curl -s https://api.github.com/repos/OceansAus/oasis3-mct/releases/latest | grep browser_download_url | cut -d: -f2- | tr -d ' "')
+curl -O ${TARFILE}
+tar -xzf $(basename ${TARFILE})
