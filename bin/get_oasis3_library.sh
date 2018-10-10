95ee1de152a47a4f3000e8df55cf4b228f019243
Aidan Heerdegen <aidan.heerdegen@gmail.com>
2018-10-10 13:26:28 +1100

    Removed tree cmd: not available on travis VM

--- bin/get_oasis3_library.sh
+++ bin/get_oasis3_library.sh
@@ -3,4 +3,5 @@ set -x
 TARFILE=$(curl -s https://api.github.com/repos/OceansAus/oasis3-mct/releases/latest | grep browser_download_url | cut -d: -f2- | tr -d ' "')
 wget ${TARFILE}
 tar -xzf $(basename ${TARFILE})
-tree -f Linux
+readlink -f Linux
+ls -R Linux
