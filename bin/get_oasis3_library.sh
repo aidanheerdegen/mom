9ade856c34c6c29e2984581b973f0b352a558cd9
Aidan Heerdegen <aidan.heerdegen@gmail.com>
2018-10-10 11:54:15 +1100

    Fixed error in download script

--- bin/get_oasis3_library.sh
+++ bin/get_oasis3_library.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl -O $(curl -s https://api.github.com/repos/OceansAus/oasis3-mct/releases/latest | grep browser_download_url | cut -d: -f2- | tr -d ' "')
-tar -xf binary_release.tar.gz
+tar -xzf binary_release.tar.gz
